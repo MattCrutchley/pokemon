05603b6a754e3317046c5c147621300654e1a067
MattCrutchley <mcrutchley8@gmail.com>
2020-02-25 23:26:24 +0000

    battle

--- pokemonpeanut.py
+++ pokemonpeanut.py
@@ -1,9 +1,11 @@
 from pokemon import pokemon
 import random
 import time
+commands = "x to attack\ns to smokescreen\nr to run"
 starter_choices = [["big bad bob", 100,60,80,"water"],["wheres my whey jay",100,80,60,"fire"]]
 spec = ["HP","Attack","Defence","Type"]
-print(starter_choices[0][0])
+smoke = 0
+xp = 0
 
 choice = input("choose your wepon\n %s \n %s \n %s \n or \n %s \n %s \n %s \n\n" %(starter_choices[0][0],spec, starter_choices[0][1:],starter_choices[1][0], spec,starter_choices[0][1:]))
 
@@ -14,8 +16,10 @@ elif choice == "wheres my whey jay":
 else:
     print("choice not recognised")
 
+
+
 battle = random.randint(0,1)
-enemies = [["big bad tadas",100,60,80,"grass"],["a wandering idiot",100, 80, 60,"water"]]
+enemies = [["big bad tadas",100,60,80,"grass"],["Random man",100, 80, 60,"water"]]
 
 
 opponent_pokemon = pokemon(enemies[battle][0],enemies[battle][1],enemies[battle][2],enemies[battle][3],enemies[battle][4])
@@ -26,39 +30,54 @@ if battle_option.lower() == "r":
     print("\nyou ran away")
 else:
     while getattr(your_pokemon, "hp") > 0:
-        cmd = input("\npress x to attack or s smokescreen")
+
+        cmd = input("\nenter command (cmd to show commands) ")
         # attack
+        while cmd.lower() == "cmd":
+            print(commands)       
+            cmd = input("\nenter command (cmd to show commands) ")
+
         if cmd.lower() == "x":
+            smoke = 0
             damage = your_pokemon.calculate_damage(getattr(your_pokemon,"type_"),getattr(opponent_pokemon,"type_"),random.randint(1,50),random.randint(1,50))
             print("\ndamage %.2f \n " %(damage))
             setattr(opponent_pokemon,"hp",getattr(opponent_pokemon,"hp") - damage)
             if getattr(opponent_pokemon,"hp") <= 0:
                 print("you killed ", opponent_pokemon.name,"\n")
+                xp += 1
+                print("you gained 1 XP!\nyour XP is now %s"%(xp))
+                break
             else:
                 print("opponents hp: %.2f " %(getattr(opponent_pokemon,"hp")),"\n")    
-
         elif cmd.lower() == "s":
-            smoke = randint(0,4)
+            smoke = random.randint(0,4)                       
             if smoke > 2:
-                print("%s is paralysed" %(getattr(opponent_pokemon,"name"))
-            else:    
-                print("%s evaded your attack"%(getattr(opponent_pokemon,"name"))
-        
-        time.sleep(1)
+                print("\n%s is paralysed" %(getattr(opponent_pokemon,"name")))
+            else:
+                print("\n%s is paralysed" %(getattr(opponent_pokemon,"name")))   
+       
+        elif cmd.lower() == "r":
+            print("you ran away")
+            break  
+        else:
+            print("\n command not recognised")     
 
+        time.sleep(2)
+        print("\n-------------------------------------------------------------------\n")
         if smoke <= 2:
             smoke = 0
             #opponent attack
-            print("\n-------------------------------------------------------------------\n")
+
             op_damage = opponent_pokemon.calculate_damage(getattr(opponent_pokemon,"type_"),getattr(your_pokemon,"type_"),random.randint(1,50),random.randint(1,50))
 
             setattr(your_pokemon,"hp",getattr(your_pokemon,"hp") - op_damage)
-            print("you are attacked, you loose %.2f " %(damage),"HP!\n")
+            print("you are attacked, you loose %.2f " %(op_damage),"HP!\n")
             if getattr(your_pokemon,"hp") <= 0:
                 print("%s killed you\n" %(enemies[battle][0]))
             else:    
                 print("your hp: %.2f "%(getattr(your_pokemon,"hp")),"\n")
             print("\n-------------------------------------------------------------------\n")   
-        
+        else:
+            print("\n%s is still paralysed" %(getattr(opponent_pokemon,"name")))
 
 
